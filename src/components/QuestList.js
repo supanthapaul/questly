6c9fd979022a30273ffbd7cfeb5f64a16548801f
jsharp008 <54524272+jsharp008@users.noreply.github.com>
2020-10-07 10:55:19 +0530

    added quest form to list

--- src/components/QuestList.js
+++ src/components/QuestList.js
@@ -6,6 +6,19 @@
 // 	setQuestList([...questList, newQuest]);
 // }
 
+import React, {useState} from 'react';
+import QuestForm from './QuestForm';
+
+export default function QuestList(){
+	const[questList,setQuestList] = useState([]);
+
+	const pushQuest= () =>{
+
+	}
+	return(
+		<QuestForm/>
+	);
+}
 
 
 
